13bf9eaaeb32e9a945219a31a0dea462bc845279
Victor Chumba <chumbavic69@gmail.com>
2026-06-19 07:36:20 +0300

    Fourth Commit

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,15 @@ import './App.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import {BrowserRouter, Routes, Route, Link} from 'react-router-dom';
 import Signup from './components/Signup';
+import Signin from './components/Signin';
+import Performance from './components/Performance';
+import Dashboard from './components/Dashboard';
+import Contact from './components/Contact';
+import About from './components/About';
+import Orders from './components/Orders';  
+import Customers from './components/Customers';
+import Products from './components/Products';
+import Costs from './components/Costs';
 
 
 function App() {
@@ -9,7 +18,7 @@ function App() {
     <BrowserRouter>
         <div className="App">
           <header className="App-header">
-            <h1>Biz-Tracker</h1>
+            <h1 className='text-dark'>Biz-Tracker</h1>
 
           </header>
 
@@ -17,10 +26,10 @@ function App() {
           <nav>
             <Link to={"/"}>DashBoard</Link>
             <Link to={"/signup"}>Sign Up</Link>
-            <Link to={"/"}>Sign In</Link>
-            <Link to={"/"}>Perfomance</Link>
-            <Link to={"/"}>Customers</Link>
-            <Link to={"/"}>Products</Link>
+            <Link to={"/signin"}>Sign In</Link>
+            <Link to={"/performance"}>Perfomance</Link>
+            <Link to={"/customers"}>Customers</Link>
+            <Link to={"/products"}>Products</Link>
             <Link to={"/costs"}>Costs</Link>
             <Link to={"/orders"}>Orders</Link>
             <Link to={"/contact"}>Contact Us</Link>
@@ -35,7 +44,7 @@ function App() {
             <Route path='/signup' element={<Signup/>}/>
             <Route path='/signin' element={<Signin/>}/>
             <Route path='/orders' element={<Orders/>}/>
-            <Route path='/perfomance' element={<Performance/>}/>
+            <Route path='/performance' element={<Performance/>}/>
             <Route path='/customers' element={<Customers/>}/>
             <Route path='/products' element={<Products/>}/>
             <Route path='/costs' element={<Costs/>}/>
---
+++ src/components/About.jsx
@@ -0,0 +1,10 @@
+const About = () => {
+
+    return(
+        <div>
+            <h4>About Us</h4>
+        </div>
+    );
+};
+
+export default About;
